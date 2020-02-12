5bfb0a77991d6e16d8268e9659b58e02e2a7b3c8
Jonathan Axford <jon.axford@outlook.com>
2020-02-11 21:59:42 -0700

    Complete

--- coupling&cohesion/prog1.java
+++ coupling&cohesion/prog1.java
@@ -1,45 +1,36 @@
 
+class A {
+	public String name; // public data member of A class
 
-class A
-{
-public String name; //public data member of A class
+	public String getName() {
 
+		// Checking a valid access to "name"
+		if (name != null)
+			return name;
+		else
+			return "not initiaized";
+	}
 
-public String getName()
-{
+	public void setName(String s) {
 
-//Checking a valid access to "name"
-if(name!=null)
-	return name;
-else
-	return"not initiaized";
+		// Checking a valid setting to "name"
+		if (s == null) {
+			System.out.println("You can't initialized name to a null");
+		}
+	}
+
 }
 
+class B {
+	public static void main(String... ar) {
+		A ob = new A();
 
-public void setName(String s)
-{
+		// Directly setting the value of data member "name" of class A, due to tight
+		// coupling between the two classes
+		ob.name = null;
 
-//Checking a valid setting to "name"
-if (s==null)
-{
-	System.out.println("You can't initialized name to a null");
-}
-}
-
-} 
-
-
-
-class B
-{
-public static void main(String... ar)
-{
-A ob= new A();
-
-//Directly setting the value of data member "name" of class A, due to tight coupling between the two classes
-ob.name=null;  
-
-//Direct access of data member "name" of class A, due to tight coupling between two classes
-System.out.println("Name is " + ob.name); 	  
-}
+		// Direct access of data member "name" of class A, due to tight coupling between
+		// two classes
+		System.out.println("Name is " + ob.name);
+	}
 }
--- coupling&cohesion/prog3.java
+++ coupling&cohesion/prog3.java
@@ -1,31 +1,39 @@
+// None of the methods in the MyReader class rely on member variables, 
+// and as such could be made static 
 class MyReader {
-    // -------------- unrelated functions
-    public boolean validateLocation(String pathIP) {
-         return ping(pathIP) && checkFTP(pathIP);
-    }
-    private boolean checkFTP(String pathIP) {
-         return true;
-    }
+     // -------------- functions related to read resource
 
-    private boolean ping(String pathIP) {
+     // read the resource from disk
+     public static String readFromDisk(String fileName) {
+          return "data of " + fileName;
+     }
+
+     // read the resource from web
+     public static String readFromWeb(String url) {
+          return "data of " + url;
+     }
+
+     // read the resource from network
+     public static String readFromNetwork(String networkAddress) {
+          return "data of " + networkAddress;
+     }
+
+}
+
+// The following unrelated methods may have use to a network reader, however
+// they are not closely related to the reader. It would be useful to move them
+// to a utility class, and make them static as they need no member variables
+class networkUtils {
+     // -------------- unrelated functions
+     public static boolean validateLocation(String pathIP) {
+          return ping(pathIP) && checkFTP(pathIP);
+     }
+
+     private static boolean checkFTP(String pathIP) {
           return true;
-    }
+     }
 
-    // -------------- functions related to read resource
-
-    // read the resource from disk
-    public String readFromDisk(String fileName) {
-         return "data of " + fileName;
-    }
-
-    // read the resource from web
-    public String readFromWeb(String url) {
-         return "data of " + url;
-    }
-
-    // read the resource from network
-    public String readFromNetwork(String networkAddress) {
-         return "data of " + networkAddress;
-    }
-
+     private static boolean ping(String pathIP) {
+          return true;
+     }
 }
